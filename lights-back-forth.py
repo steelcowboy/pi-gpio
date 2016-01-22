7ecf9f3ad459f4b81bd1fc695bd543d501ecd15a
steelcowboy <jhdoubleoseven@gmail.com>
2016-01-22 13:28:32 -0800

    Playing with threading

--- lights-back-forth.py
+++ lights-back-forth.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python3
 
 import RPi.GPIO as GPIO
-import time
+import time, sys
 from itertools import cycle
-import sys
+from threading import Thread
 import mypins
+
 
 GPIO.setmode(GPIO.BCM)
 
@@ -19,16 +20,25 @@ def cycle_pin(pin):
     GPIO.output(pin, GPIO.HIGH)
     time.sleep(0.1)
     GPIO.output(pin, GPIO.LOW)
-    
+
+def runLEDs():
+    for x in c:
+        cycle_pin(x)
+
 for i in pins[::-1]:
     pins.append(i)
 
 c = cycle(pins)
 
 try:
-    for x in c:
-        cycle_pin(x)
-
+    t1 = Thread(target=runLEDs)
+    t1.daemon = True
+    t1.start()
+    while True:
+        #if GPIO.input(pins[0]):
+        #print("Hit the first one!")
+        time.sleep(1)
 except KeyboardInterrupt:
+    t1.join()
     GPIO.output(pins, GPIO.LOW)
     sys.exit(0)
